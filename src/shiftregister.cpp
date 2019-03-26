e37e8b923eef0f8ee738bdb168efcc19f7753e10
JohannesWilde <git@jowil.de>
2019-03-26 17:35:15 +0100

    Correct comments about offset calculation in shiftInBits.

--- src/shiftregister.cpp
+++ src/shiftregister.cpp
@@ -45,10 +45,11 @@ void ShiftRegister::shiftInBits(uint8_t const * const bitStreamArray) const
 {
     if ( length > 0 )
     {
-        // Because of integer division, one byte more will have to be used [unless length is a multiple of 8]
+        // Because of integer division, one byte more will have to be used [unless length is a multiple of 8],
+        // this "+1" however is compensated by the fact, that the offset in the array starts at 0 and not 1.
         uint8_t const curByteNumber = ( (length - 1) >> 3 ); // = divide by 8 [integer!]
         // Now find out, how many of the bits in the most significant byte will be used.
-        // (length - ( (curByteNumber - 1) * 8 ) is the number of bits in the most significant byte;
+        // (length - ( curByteNumber * 8 ) is the number of bits in the most significant byte;
         // because 0x01 has a bit at the first position alreade, subtract 1.
         uint8_t curBitMask = static_cast<uint8_t>(0x01 << (length - ( curByteNumber << 3 ) - 1));
 
@@ -56,7 +57,7 @@ void ShiftRegister::shiftInBits(uint8_t const * const bitStreamArray) const
         while (bitStreamArrayByte >= bitStreamArray)
         {
             // Because above calculation causes the last bit to be bit 0 [LSB] of Byte 0 [LSBy], I don't have
-            // to check in the following while-loop, whether I don't try to read to many bits.
+            // to check in the following while-loop, whether I don't try to read too many bits.
             while (curBitMask > 0)
             {
                 ( ((*bitStreamArrayByte) & curBitMask) != 0 ) ? serialInput->setType(AvrPin::OUTPUT_HIGH) : serialInput->setType(AvrPin::OUTPUT_LOW);
