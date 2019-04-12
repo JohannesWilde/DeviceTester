f99b36631976253eecf0817451fd06901d898df4
JohannesWilde <git@jowil.de>
2019-04-12 15:47:54 +0200

    Add template version of DeviceTester struct.

--- src/devicetester.hpp
+++ src/devicetester.hpp
@@ -2,14 +2,53 @@
 #define DEVICETESTER_HPP
 
 #include <stdint.h>
+#include <util/delay.h>
 #include "avrinputoutput.hpp"
+#include "avrpin.hpp"
 
-// forward declarations
-class AvrPin;
 
-class DeviceTester
+template<typename ButtonPin_,
+         typename LedPin_,
+         typename DevicePin0_,
+         typename DevicePin1_,
+         typename DevicePin2_,
+         typename DevicePin3_,
+         typename DevicePin4_,
+         typename DevicePin5_,
+         typename DevicePin6_,
+         typename DevicePin7_,
+         typename DevicePin8_,
+         typename DevicePin9_,
+         typename DevicePin10_,
+         typename DevicePin11_,
+         typename DevicePin12_,
+         typename DevicePin13_,
+         typename DevicePin14_,
+         typename DevicePin15_ >
+struct DeviceTester
 {
 public:
+    // make the pins publically available
+    typedef LedPin_ LedPin;
+    typedef ButtonPin_ ButtonPin;
+    typedef DevicePin0_ DevicePin0;
+    typedef DevicePin1_ DevicePin1;
+    typedef DevicePin2_ DevicePin2;
+    typedef DevicePin3_ DevicePin3;
+    typedef DevicePin4_ DevicePin4;
+    typedef DevicePin5_ DevicePin5;
+    typedef DevicePin6_ DevicePin6;
+    typedef DevicePin7_ DevicePin7;
+    typedef DevicePin8_ DevicePin8;
+    typedef DevicePin9_ DevicePin9;
+    typedef DevicePin10_ DevicePin10;
+    typedef DevicePin11_ DevicePin11;
+    typedef DevicePin12_ DevicePin12;
+    typedef DevicePin13_ DevicePin13;
+    typedef DevicePin14_ DevicePin14;
+    typedef DevicePin15_ DevicePin15;
+
+
     // This DeviceTester is for 16-pin devices and additionally has a button [which uses an internal pullup
     // and when pressed should connect to ground] and a port to en-/disable the leds.
     // The pin type is from the perspective of the tester, i.e. if the device under test:
@@ -17,59 +56,103 @@ public:
     //  - requires GND then use OUTPUT_LOW
     //  - has an output itself then use INPUT or INPUT_PULLUP [depending on the device under test]
     // for that pin.
-    DeviceTester(AvrPin const * const button,
-                 AvrPin const * const led,
-                 AvrPin const * const devicePin0,
-                 AvrInputOutput::PinType devicePin0Type,
-                 AvrPin const * const devicePin1,
-                 AvrInputOutput::PinType devicePin1Type,
-                 AvrPin const * const devicePin2,
-                 AvrInputOutput::PinType devicePin2Type,
-                 AvrPin const * const devicePin3,
-                 AvrInputOutput::PinType devicePin3Type,
-                 AvrPin const * const devicePin4,
-                 AvrInputOutput::PinType devicePin4Type,
-                 AvrPin const * const devicePin5,
-                 AvrInputOutput::PinType devicePin5Type,
-                 AvrPin const * const devicePin6,
-                 AvrInputOutput::PinType devicePin6Type,
-                 AvrPin const * const devicePin7,
-                 AvrInputOutput::PinType devicePin7Type,
-                 AvrPin const * const devicePin8,
-                 AvrInputOutput::PinType devicePin8Type,
-                 AvrPin const * const devicePin9,
-                 AvrInputOutput::PinType devicePin9Type,
-                 AvrPin const * const devicePin10,
-                 AvrInputOutput::PinType devicePin10Type,
-                 AvrPin const * const devicePin11,
-                 AvrInputOutput::PinType devicePinT11ype,
-                 AvrPin const * const devicePin12,
-                 AvrInputOutput::PinType devicePin12Type,
-                 AvrPin const * const devicePin13,
-                 AvrInputOutput::PinType devicePin13Type,
-                 AvrPin const * const devicePin14,
-                 AvrInputOutput::PinType devicePin14Type,
-                 AvrPin const * const devicePin15,
-                 AvrInputOutput::PinType devicePin15Type);
+    static void initialize(AvrInputOutput::PinType devicePin0Type,
+                             AvrInputOutput::PinType devicePin1Type,
+                             AvrInputOutput::PinType devicePin2Type,
+                             AvrInputOutput::PinType devicePin3Type,
+                             AvrInputOutput::PinType devicePin4Type,
+                             AvrInputOutput::PinType devicePin5Type,
+                             AvrInputOutput::PinType devicePin6Type,
+                             AvrInputOutput::PinType devicePin7Type,
+                             AvrInputOutput::PinType devicePin8Type,
+                             AvrInputOutput::PinType devicePin9Type,
+                             AvrInputOutput::PinType devicePin10Type,
+                             AvrInputOutput::PinType devicePin11Type,
+                             AvrInputOutput::PinType devicePin12Type,
+                             AvrInputOutput::PinType devicePin13Type,
+                             AvrInputOutput::PinType devicePin14Type,
+                             AvrInputOutput::PinType devicePin15Type)
+    {
+        LedPin_::setType(AvrInputOutput::OUTPUT_LOW); // leds disabled by default
+        ButtonPin_::setType(AvrInputOutput::INPUT_PULLUP); // internal pull-up required!
+        DevicePin0_::setType(devicePin0Type);
+        DevicePin1_::setType(devicePin1Type);
+        DevicePin2_::setType(devicePin2Type);
+        DevicePin3_::setType(devicePin3Type);
+        DevicePin4_::setType(devicePin4Type);
+        DevicePin5_::setType(devicePin5Type);
+        DevicePin6_::setType(devicePin6Type);
+        DevicePin7_::setType(devicePin7Type);
+        DevicePin8_::setType(devicePin8Type);
+        DevicePin9_::setType(devicePin9Type);
+        DevicePin10_::setType(devicePin10Type);
+        DevicePin11_::setType(devicePin11Type);
+        DevicePin12_::setType(devicePin12Type);
+        DevicePin13_::setType(devicePin13Type);
+        DevicePin14_::setType(devicePin14Type);
+        DevicePin15_::setType(devicePin15Type);
+    }
 
-    void enableLeds() const;
-    void disableLeds() const;
+    static void enableLeds()
+    {
+        LedPin_::setPort();
+    }
+
+    static void disableLeds()
+    {
+        LedPin_::clearPort();
+    }
 
     // wait until button is pressed
-    void waitForButtonPressAndRelease() const;
+    static void waitForButtonPressAndRelease()
+    {
+        // this method checks, that the pin is LOW and afterwards goes HIGH again.
+        bool wasLowAtLeastOnce = false;
+        AvrInputOutput::PinState curState = AvrInputOutput::HIGH;
+        do
+        {
+            // try to circumvent debounce by waiting a small amount of time between asking again
+            _delay_ms(10);
+            curState = ButtonPin_::readPin();
+            if (curState == AvrInputOutput::LOW)
+            {
+                wasLowAtLeastOnce = true;
+            }
+        }
+        while (!(wasLowAtLeastOnce && (AvrInputOutput::HIGH == curState)));
+    }
 
     // wait until button was pressed at least once
-    void waitForButtonPress() const;
+    static void waitForButtonPress()
+    {
+        // this method checks, that the pin is LOW
+        AvrInputOutput::PinState curState = AvrInputOutput::HIGH;
+        do
+        {
+            // try to circumvent debounce by waiting a small amount of time between asking again
+            _delay_ms(10);
+            curState = ButtonPin_::readPin();
+        }
+        while (!(AvrInputOutput::LOW == curState));
+    }
 
-    bool checkDevicePinPin(uint8_t const pinNumber, AvrInputOutput::PinState const pinState) const;
-    bool checkDevicePinPort(uint8_t const pinNumber, AvrInputOutput::PinState const pinState) const;
-    bool checkDevicePinDdr(uint8_t const pinNumber, AvrInputOutput::PinState const pinState) const;
+    template<typename DevicePin>
+    static bool checkDevicePinPin(AvrInputOutput::PinState const pinState)
+    {
+        return ( DevicePin::readPin() == pinState );
+    }
 
-private:
-    AvrPin const * const pinButton_;
-    AvrPin const * const pinLed_;
-    // this array has non-const members only, because the C++ standard before C++11 does not allow for its initialization
-    AvrPin const * pinsDevice_[16];
+    template<typename DevicePin>
+    static bool checkDevicePinPort(AvrInputOutput::PinState const pinState)
+    {
+        return ( DevicePin::readPort() == pinState );
+    }
+
+    template<typename DevicePin>
+    static bool checkDevicePinDdr(AvrInputOutput::PinState const pinState)
+    {
+        return ( DevicePin::readDdr() == pinState );
+    }
 };
 
 #endif // DEVICETESTER_H
