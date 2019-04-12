3432998d86b8c15813be84f8a221efd57f4ab3ed
JohannesWilde <git@jowil.de>
2019-04-12 16:34:59 +0200

    Add ShiftRegisterTester - the funny thing about the current main routine is, that one just has to short-circuit two parallel outputs and at some point the arduino thus will break the routine.

--- src/main.cpp
+++ src/main.cpp
@@ -7,35 +7,65 @@
 #include <util/delay.h>
 #include "arduinouno.hpp"
 #include "shiftregister.hpp"
+#include "shiftregistertester.hpp"
 
 
 int main()
 {
     typedef ArduinoUno arduinoUno;
+    typedef ShiftRegisterTester<arduinoUno::A4,
+                                arduinoUno::A5,
+                                arduinoUno::D2,
+                                arduinoUno::D3,
+                                arduinoUno::D4,
+                                arduinoUno::D5,
+                                arduinoUno::D6,
+                                arduinoUno::D7,
+                                arduinoUno::D8,
+                                arduinoUno::D9,
+                                arduinoUno::D10,
+                                arduinoUno::D11,
+                                arduinoUno::D12,
+                                arduinoUno::D13,
+                                arduinoUno::A0,
+                                arduinoUno::A1,
+                                arduinoUno::A2,
+                                arduinoUno::A3> shiftRegisterTester;
 
-    arduinoUno::D2::setType(AvrInputOutput::INPUT);
-    arduinoUno::D3::setType(AvrInputOutput::INPUT);
-    arduinoUno::D4::setType(AvrInputOutput::INPUT);
-    arduinoUno::D5::setType(AvrInputOutput::INPUT);
-    arduinoUno::D6::setType(AvrInputOutput::INPUT);
-    arduinoUno::D7::setType(AvrInputOutput::INPUT);
-    arduinoUno::D8::setType(AvrInputOutput::INPUT);
-    arduinoUno::A2::setType(AvrInputOutput::INPUT);
+    shiftRegisterTester::initialize();
 
-    arduinoUno::D9::setType(AvrInputOutput::OUTPUT_LOW);
-    arduinoUno::A3::setType(AvrInputOutput::OUTPUT_HIGH);
-
-    typedef ShiftRegister<2, arduinoUno::A1, arduinoUno::D12, arduinoUno::D13, arduinoUno::A0, arduinoUno::D11> shiftRegister;
+    typedef ShiftRegister<8, arduinoUno::A1, arduinoUno::D12, arduinoUno::D13, arduinoUno::A0, arduinoUno::D11> shiftRegister;
     shiftRegister::initialize();
     uint8_t data[1] = {0xff};
     shiftRegister::enableOutput();
 
-    arduinoUno::A5::setType(AvrInputOutput::OUTPUT_HIGH); // enable leds on device tester
-    while (true)
+    data[0] = 0xff;
+    shiftRegister::shiftInBits(data);
+    shiftRegister::showShiftRegister();
+    _delay_ms(1000);
+
+    shiftRegisterTester::enableLeds();
+    _delay_ms(1000);
+
+    if ( shiftRegisterTester::checkOutputEnabled() )
     {
-        _delay_ms(100);
+        data[0] = 0x55;
         shiftRegister::shiftInBits(data);
         shiftRegister::showShiftRegister();
+    }
+
+
+    while (true)
+    {
+        _delay_ms(500);
+        shiftRegister::shiftInBits(data);
+        shiftRegister::showShiftRegister();
+
+        if (!shiftRegisterTester::checkParallelOutput(data[0]))
+        {
+            break;
+        }
+
         data[0]--;
     }
 
--- src/shiftregistertester.hpp
+++ src/shiftregistertester.hpp
@@ -3,60 +3,174 @@
 
 #include "devicetester.hpp"
 #include "avrinputoutput.hpp"
+#include "avrpin.hpp"
 
-class ShiftRegisterTester : public DeviceTester
+template< typename Button_,
+            typename Led_,
+            typename ParallelOutput1_,
+            typename ParallelOutput2_,
+            typename ParallelOutput3_,
+            typename ParallelOutput4_,
+            typename ParallelOutput5_,
+            typename ParallelOutput6_,
+            typename ParallelOutput7_,
+            typename Gnd_,
+            typename SerialOutput_,
+            typename InvertedShiftRegisterClear_,
+            typename ShiftRegisterClock_,
+            typename ShowRegisterClock_,
+            typename InvertedOutputEnable_,
+            typename SerialInput_,
+            typename ParallelOutput0_,
+            typename Vcc_>
+struct ShiftRegisterTester : public DeviceTester<Button_,
+                                                Led_,
+                                                ParallelOutput1_,
+                                                ParallelOutput2_,
+                                                ParallelOutput3_,
+                                                ParallelOutput4_,
+                                                ParallelOutput5_,
+                                                ParallelOutput6_,
+                                                ParallelOutput7_,
+                                                Gnd_,
+                                                SerialOutput_,
+                                                InvertedShiftRegisterClear_,
+                                                ShiftRegisterClock_,
+                                                ShowRegisterClock_,
+                                                InvertedOutputEnable_,
+                                                SerialInput_,
+                                                ParallelOutput0_,
+                                                Vcc_>
 {
+protected:
+    typedef DeviceTester<Button_,
+                        Led_,
+                        ParallelOutput1_,
+                        ParallelOutput2_,
+                        ParallelOutput3_,
+                        ParallelOutput4_,
+                        ParallelOutput5_,
+                        ParallelOutput6_,
+                        ParallelOutput7_,
+                        Gnd_,
+                        SerialOutput_,
+                        InvertedShiftRegisterClear_,
+                        ShiftRegisterClock_,
+                        ShowRegisterClock_,
+                        InvertedOutputEnable_,
+                        SerialInput_,
+                        ParallelOutput0_,
+                        Vcc_> DeviceTester_;
 public:
-    // These values are specific for the used hardware [74HC595].
-    // Please note, that changing these won't suffice, but that the constructor will
-    // have to be adapted as well.
-    enum PinNumber {
-        PARALLEL_OUTPUT_1 = 0,
-        PARALLEL_OUTPUT_2 = 1,
-        PARALLEL_OUTPUT_3 = 2,
-        PARALLEL_OUTPUT_4 = 3,
-        PARALLEL_OUTPUT_5 = 4,
-        PARALLEL_OUTPUT_6 = 5,
-        PARALLEL_OUTPUT_7 = 6,
-        GND = 7,
-        SERIAL_OUTPUT = 8,
-        INVERTED_SHIFT_REGISTER_CLEAR = 9,
-        SHIFT_REGISTER_CLOCK = 10,
-        SHOW_REGISTER_CLOCK = 11,
-        INVERTED_OUTPUT_ENABLE = 12,
-        SERIAL_INPUT = 13,
-        PARALLEL_OUTPUT_0 = 14,
-        VCC = 15
-    };
+    // make the pins publically available
+    typedef ParallelOutput1_ ParallelOutput1Pin;
+    typedef ParallelOutput2_ ParallelOutput2Pin;
+    typedef ParallelOutput3_ ParallelOutput3Pin;
+    typedef ParallelOutput4_ ParallelOutput4Pin;
+    typedef ParallelOutput5_ ParallelOutput5Pin;
+    typedef ParallelOutput6_ ParallelOutput6Pin;
+    typedef ParallelOutput7_ ParallelOutput7Pin;
+    typedef Gnd_ GndPin;
+    typedef SerialOutput_ SerialOutputPin;
+    typedef InvertedShiftRegisterClear_ InvertedShiftRegisterClearPin;
+    typedef ShiftRegisterClock_ ShiftRegisterClockPin;
+    typedef ShowRegisterClock_ ShowRegisterClockPin;
+    typedef InvertedOutputEnable_ InvertedOutputEnablePin;
+    typedef SerialInput_ SerialInputPin;
+    typedef ParallelOutput0_ ParallelOutput0Pin;
+    typedef Vcc_ VccPin;
 
-    ShiftRegisterTester(AvrPin const * const button,
-                        AvrPin const * const led,
-                        AvrPin const * const parallelOutput1,
-                        AvrPin const * const parallelOutput2,
-                        AvrPin const * const parallelOutput3,
-                        AvrPin const * const parallelOutput4,
-                        AvrPin const * const parallelOutput5,
-                        AvrPin const * const parallelOutput6,
-                        AvrPin const * const parallelOutput7,
-                        AvrPin const * const gnd,
-                        AvrPin const * const serialOutput,
-                        AvrPin const * const invertedShiftRegisterClear,
-                        AvrPin const * const shiftRegisterClock,
-                        AvrPin const * const showRegisterClock,
-                        AvrPin const * const invertedOutputEnable,
-                        AvrPin const * const serialInput,
-                        AvrPin const * const parallelOutput0,
-                        AvrPin const * const vcc);
+    // this will reconfigure the ports - so set the sheftRegister driver after having called this.
+    static void initialize()
+    {
+        DeviceTester_::initialize(AvrInputOutput::INPUT,
+                                  AvrInputOutput::INPUT,
+                                  AvrInputOutput::INPUT,
+                                  AvrInputOutput::INPUT,
+                                  AvrInputOutput::INPUT,
+                                  AvrInputOutput::INPUT,
+                                  AvrInputOutput::INPUT,
+                                  AvrInputOutput::OUTPUT_LOW,   // Gnd
+                                  AvrInputOutput::INPUT,        // SerialOutput
+                                  AvrInputOutput::OUTPUT_HIGH,  // InvertedShiftRegisterClear
+                                  AvrInputOutput::OUTPUT_HIGH,  // ShiftRegisterClock
+                                  AvrInputOutput::OUTPUT_HIGH,  // ShowRegisterClock
+                                  AvrInputOutput::OUTPUT_HIGH,  // InvertedOutputEnable_
+                                  AvrInputOutput::OUTPUT_LOW,   // SerialInput
+                                  AvrInputOutput::INPUT,
+                                  AvrInputOutput::OUTPUT_HIGH   // Vcc
+                                  );
+    }
 
-    bool checkOutputEnabled() const;
-    bool checkInvertedShiftRegisterClear(AvrInputOutput::PinState const pinState) const;
-    bool checkShiftRegisterClock(AvrInputOutput::PinState const pinState) const;
-    bool checkShowRegisterClock(AvrInputOutput::PinState const pinState) const;
-    bool checkSerialOutput(AvrInputOutput::PinState const pinState) const;
-    bool checkSerialInput(AvrInputOutput::PinState const pinState) const;
+    static bool checkOutputEnabled()
+    {
+        return DeviceTester_::template checkDevicePinPort<InvertedOutputEnable_>(AvrInputOutput::LOW);
+    }
+
+    static bool checkInvertedShiftRegisterClearing()
+    {
+        return DeviceTester_::template checkDevicePinPort<InvertedShiftRegisterClear_>(AvrInputOutput::LOW);
+    }
+
+    static bool checkOutputEnable(AvrInputOutput::PinState const pinState)
+    {
+        return DeviceTester_::template checkDevicePinPort<InvertedOutputEnable_>(pinState);
+    }
+
+    static bool checkInvertedShiftRegisterClear(AvrInputOutput::PinState const pinState)
+    {
+        return DeviceTester_::template checkDevicePinPort<InvertedShiftRegisterClear_>(pinState);
+    }
+
+    static bool checkShiftRegisterClock(AvrInputOutput::PinState const pinState)
+    {
+        return DeviceTester_::template checkDevicePinPort<ShiftRegisterClock_>(pinState);
+    }
+
+    static bool checkShowRegisterClock(AvrInputOutput::PinState const pinState)
+    {
+        return DeviceTester_::template checkDevicePinPort<ShowRegisterClock_>(pinState);
+    }
+
+    static bool checkSerialOutput(AvrInputOutput::PinState const pinState)
+    {
+        return DeviceTester_::template checkDevicePinPort<SerialOutput_>(pinState);
+    }
+
+    static bool checkSerialInput(AvrInputOutput::PinState const pinState)
+    {
+        return DeviceTester_::template checkDevicePinPort<SerialInput_>(pinState);
+    }
+
+    static bool checkParallelOutput(uint8_t const expectedBitMask)
+    {
+        return 0x00 == compareParallelOutputTo(expectedBitMask);
+    }
+
     // Returns an 8-bit bit mask where a 1 denotes mismatch and 0 denotes matching of expectation
     // and measured.
-    uint8_t checkParallelOutput(uint8_t const expectedBitMask) const;
+    static uint8_t compareParallelOutputTo(uint8_t const expectedBitMask)
+    {
+        uint8_t const curBitMask = readParallelOutput();
+        uint8_t const unexpectedBitMask = curBitMask ^ expectedBitMask;
+        return unexpectedBitMask;
+    }
+
+    static uint8_t readParallelOutput()
+    {
+        // disable interrupts?
+        uint8_t currentBitMask = 0x00;
+        currentBitMask |= static_cast<uint8_t>(ParallelOutput0_::readPin()) << 0;
+        currentBitMask |= static_cast<uint8_t>(ParallelOutput1_::readPin()) << 1;
+        currentBitMask |= static_cast<uint8_t>(ParallelOutput2_::readPin()) << 2;
+        currentBitMask |= static_cast<uint8_t>(ParallelOutput3_::readPin()) << 3;
+        currentBitMask |= static_cast<uint8_t>(ParallelOutput4_::readPin()) << 4;
+        currentBitMask |= static_cast<uint8_t>(ParallelOutput5_::readPin()) << 5;
+        currentBitMask |= static_cast<uint8_t>(ParallelOutput6_::readPin()) << 6;
+        currentBitMask |= static_cast<uint8_t>(ParallelOutput7_::readPin()) << 7;
+        // enable interrupts?
+        return currentBitMask;
+    }
 
 };
 
