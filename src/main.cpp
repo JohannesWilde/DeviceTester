7a2070b187a2f7909cecff5823db173a902a3d05
JohannesWilde <git@jowil.de>
2019-04-14 12:38:14 +0200

    Add driver and tester for single latch.

--- src/main.cpp
+++ src/main.cpp
@@ -7,11 +7,11 @@
 #include <util/delay.h>
 #include "arduinouno.hpp"
 
-#include "shiftregister.hpp"
-#include "shiftregistertester.hpp"
+//#include "shiftregister.hpp"
+//#include "shiftregistertester.hpp"
 
-//#include "rslatch.hpp"
-//#include "rslatchtester.hpp"
+#include "rslatch.hpp"
+#include "rslatchtester.hpp"
 
 int main()
 {
@@ -35,16 +35,26 @@ int main()
                         arduinoUno::A2,
                         arduinoUno::A3> deviceTester;
 
-//    typedef RsLatch<deviceTester::DevicePin3,
-//                    deviceTester::DevicePin2,
-//                    deviceTester::DevicePin4> rsLatch;
+    typedef RsLatch<deviceTester::DevicePin3,
+                    deviceTester::DevicePin2,
+                    deviceTester::DevicePin4> rsLatch;
 
-//    typedef RsLatchTester<deviceTester::ButtonPin,
-//                          deviceTester::LedPin,
-//                          deviceTester::DevicePin7,
-//                          deviceTester::DevicePin15,
-//                          deviceTester::DevicePin1,
-//                          rsLatch> rsLatchTester;
+    typedef RsLatchTester<deviceTester::ButtonPin,
+                          deviceTester::LedPin,
+                          deviceTester::DevicePin7,
+                          deviceTester::DevicePin15,
+                          deviceTester::DevicePin1,
+                          rsLatch> rsLatchTester;
+    rsLatchTester::initialize();
+    rsLatch::initialize();
+
+    while (true)
+    {
+        rsLatchTester::waitForButtonPressAndRelease();
+        rsLatchTester::TestResult testResult = rsLatchTester::test();
+        rsLatchTester::showTestResult(testResult);
+    }
+
 
 //    while (true)
 //    {
@@ -52,30 +62,30 @@ int main()
 //        rsLatchTester::waitForButtonPressAndRelease();
 //    }
 
-    typedef ShiftRegister<8, deviceTester::DevicePin13, deviceTester::DevicePin10, deviceTester::DevicePin11, deviceTester::DevicePin12, deviceTester::DevicePin9> shiftRegister;
-    typedef ShiftRegisterTester<arduinoUno::A4,
-                                arduinoUno::A5,
-                                arduinoUno::D2,
-                                arduinoUno::D3,
-                                arduinoUno::D4,
-                                arduinoUno::D5,
-                                arduinoUno::D6,
-                                arduinoUno::D7,
-                                arduinoUno::D8,
-                                arduinoUno::D9,
-                                arduinoUno::D10,
-                                arduinoUno::A2,
-                                arduinoUno::A3,
-                                shiftRegister> shiftRegisterTester;
-    shiftRegisterTester::initialize();
-    shiftRegister::initialize();
+//    typedef ShiftRegister<8, deviceTester::DevicePin13, deviceTester::DevicePin10, deviceTester::DevicePin11, deviceTester::DevicePin12, deviceTester::DevicePin9> shiftRegister;
+//    typedef ShiftRegisterTester<arduinoUno::A4,
+//                                arduinoUno::A5,
+//                                arduinoUno::D2,
+//                                arduinoUno::D3,
+//                                arduinoUno::D4,
+//                                arduinoUno::D5,
+//                                arduinoUno::D6,
+//                                arduinoUno::D7,
+//                                arduinoUno::D8,
+//                                arduinoUno::D9,
+//                                arduinoUno::D10,
+//                                arduinoUno::A2,
+//                                arduinoUno::A3,
+//                                shiftRegister> shiftRegisterTester;
+//    shiftRegisterTester::initialize();
+//    shiftRegister::initialize();
 
-    while (true)
-    {
-        shiftRegisterTester::waitForButtonPressAndRelease();
-        shiftRegisterTester::TestResult testResult = shiftRegisterTester::test();
-        shiftRegisterTester::showTestResult(testResult);
-    }
+//    while (true)
+//    {
+//        shiftRegisterTester::waitForButtonPressAndRelease();
+//        shiftRegisterTester::TestResult testResult = shiftRegisterTester::test();
+//        shiftRegisterTester::showTestResult(testResult);
+//    }
 
     return 0;
 }
